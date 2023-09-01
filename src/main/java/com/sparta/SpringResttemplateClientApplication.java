733679edd19ec1348e98975fee4edf888dfda24a
seungtaemoon <Robbie@email.com>
2023-09-02 01:56:44 +0900

    Init commit

---
+++ src/main/java/com/sparta/SpringResttemplateClientApplication.java
@@ -0,0 +1,13 @@
+package com.sparta;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringResttemplateClientApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringResttemplateClientApplication.class, args);
+    }
+
+}
